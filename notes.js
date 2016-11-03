3467e5f70e8b1880dc56ada9a5187d66ed381384
ayalalaura <ayala.lauramichelle@gmail.com>
2016-11-03 16:52:58 -0400

    Add d14 Project1 updated

---
+++ notes.js
@@ -0,0 +1,40 @@
+// HAZEL THE HEALTH NUT
+// Your gal pal Hazel is ravenously hungry! Feed her her favorite healthy snacks before she gets hangry!
+// But watch out for the junk food, she's super healthy and gets....gassy.
+
+// MVP:
+// - functional game flow where foods fall; bad foods are totaled into junk-o-meter; good foods add to total score
+// - when clicked on, foods give respective scores
+// - win - set score (25,000) /timed (60 seconds)
+// - lose - junk-o-meter overflows (score? how is this totaled?)
+
+// Reach:
+// - effects when both types of foods are clicked on
+// - audio
+// - cohesive styling
+// - staus boxes that slide in and out of sight when triggered
+// - high score board (local JS storage)
+// - restart option
+
+// Objective:
+// - Reach 5,000 before time runs out!
+// - Beware of the junk! Click on too much junk food and Hazel gets sick (measured by junk-o-meter)
+// - Speed increases every 1,000 points, so keep an eye on  your score bar
+
+// Technical Requirements
+
+// Your app must:
+
+// Render the game in the browser
+// Design and implement logic for winning & visually display whether the user wins or loses
+// Include separate HTML / CSS / JavaScript files
+// Use the KISS (Keep It Simple Stupid) and DRY (Don't Repeat Yourself) principle
+// Use jQuery for DOM manipulation
+// Deploy your game online, where the rest of the world can access it
+
+// Necessary Deliverables:
+
+// A working game, built by you, hosted on the internet
+// A git repository hosted on Github, with a link to your hosted game in the URL section, and frequent commits dating back to the very beginning of the project
+// You will want to create a git repository on your local machine in a different directory and OUTSIDE of the repo for this class.
+// A readme.md file with explanations of the technologies used, the approach taken, installation instructions, unsolved problems, etc.
